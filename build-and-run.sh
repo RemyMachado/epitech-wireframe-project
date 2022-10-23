790ff9763ffc0baac46bf5282d8b19f1d4d8dc45
RemyMachado <remy@machado.tech>
2022-10-23 18:53:34 +0200

    feat: color gradient square matrix

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,3 +2,4 @@
 
 rm a.out
 gcc -Wall -Wextra src/*.c -lcsfml-graphics -lcsfml-window
+./a.out
--- src/main.c
+++ src/main.c
@@ -28,6 +28,7 @@ void framebuffer_destroy(struct framebuffer *framebuffer) {
     free(framebuffer);
 }
 
+// TODO: consider adding a guard to prevent x,y overflow
 void my_put_pixel(struct framebuffer *framebuffer, unsigned int x, unsigned int y, sfColor color) {
     const int index = (y * framebuffer->width) * 4 + x * 4;
 
@@ -42,6 +43,59 @@ void my_put_square(struct framebuffer *framebuffer, unsigned int size, unsigned 
         for (unsigned int j = 0; j < size; ++j) {
             my_put_pixel(framebuffer, x + j, y + i, color);
         }
+    }
+}
+
+void my_put_square_line(struct framebuffer *framebuffer, unsigned int size, unsigned int quantity, unsigned int x,
+                        unsigned int y, sfColor leftColor, sfColor rightColor) {
+    for (unsigned int i = 0; i < quantity; ++i) {
+        sfUint8 newLeftR = leftColor.r - leftColor.r / quantity * i;
+        sfUint8 newLeftG = leftColor.g - leftColor.g / quantity * i;
+        sfUint8 newLeftB = leftColor.b - leftColor.b / quantity * i;
+        sfUint8 newLeftA = leftColor.a - leftColor.a / quantity * i;
+
+        sfUint8 newRightR = rightColor.r / quantity * i;
+        sfUint8 newRightG = rightColor.g / quantity * i;
+        sfUint8 newRightB = rightColor.b / quantity * i;
+        sfUint8 newRightA = rightColor.a / quantity * i;
+
+        sfColor newColor = {newLeftR + newRightR, newLeftG + newRightG, newLeftB + newRightB, newLeftA + newRightA};
+
+        my_put_square(framebuffer, size, x + i * size, y, newColor);
+    }
+}
+
+void my_put_square_matrix(struct framebuffer *framebuffer, unsigned int squareSize, unsigned int matrixSize,
+                          unsigned int x, unsigned int y, sfColor topLeftColor, sfColor topRightColor,
+                          sfColor bottomRightColor, sfColor bottomLeftColor) {
+    for (unsigned int i = 0; i < matrixSize; ++i) {
+        sfUint8 newTopLeftR = topLeftColor.r - topLeftColor.r / matrixSize * i;
+        sfUint8 newTopLeftG = topLeftColor.g - topLeftColor.g / matrixSize * i;
+        sfUint8 newTopLeftB = topLeftColor.b - topLeftColor.b / matrixSize * i;
+        sfUint8 newTopLeftA = topLeftColor.a - topLeftColor.a / matrixSize * i;
+
+        sfUint8 newBottomLeftR = bottomLeftColor.r / matrixSize * i;
+        sfUint8 newBottomLeftG = bottomLeftColor.g / matrixSize * i;
+        sfUint8 newBottomLeftB = bottomLeftColor.b / matrixSize * i;
+        sfUint8 newBottomLeftA = bottomLeftColor.a / matrixSize * i;
+
+        sfUint8 newTopRightR = topRightColor.r - topRightColor.r / matrixSize * i;
+        sfUint8 newTopRightG = topRightColor.g - topRightColor.g / matrixSize * i;
+        sfUint8 newTopRightB = topRightColor.b - topRightColor.b / matrixSize * i;
+        sfUint8 newTopRightA = topRightColor.a - topRightColor.a / matrixSize * i;
+
+        sfUint8 newBottomRightR = bottomRightColor.r / matrixSize * i;
+        sfUint8 newBottomRightG = bottomRightColor.g / matrixSize * i;
+        sfUint8 newBottomRightB = bottomRightColor.b / matrixSize * i;
+        sfUint8 newBottomRightA = bottomRightColor.a / matrixSize * i;
+
+        sfColor newLeftColor = {newTopLeftR + newBottomLeftR, newTopLeftG + newBottomLeftG,
+                                newTopLeftB + newBottomLeftB, newTopLeftA + newBottomLeftA};
+
+        sfColor newRightColor = {newTopRightR + newBottomRightR, newTopRightG + newBottomRightG,
+                                 newTopRightB + newBottomRightB, newTopRightA + newBottomRightA};
+
+        my_put_square_line(framebuffer, squareSize, matrixSize, x, y + (squareSize * i), newLeftColor, newRightColor);
     }
 }
 
@@ -71,8 +125,6 @@ int main() {
     /* Limit the window framerate */
     sfRenderWindow_setFramerateLimit(window, FRAMERATE_LIMIT);
 
-    printf("Hello, World!\n");
-
     while (sfRenderWindow_isOpen(window)) {
         sfRenderWindow_display(window);
 
@@ -84,7 +136,11 @@ int main() {
 
         /* Draw the pixel at (100,100) in the framebuffer */
         //        my_put_pixel(framebuffer, 100, 100, sfRed);
-        my_put_square(framebuffer, 400, 500, 500, sfBlue);
+        //        my_put_square(framebuffer, 300, 500, 200, sfRed);
+        //        my_put_square_line(framebuffer, 150, 10, 0, 0, sfRed, sfBlue);
+        //        my_put_square_matrix(framebuffer, 90, 10, 0, 0, sfRed, sfBlue, sfCyan, sfYellow);
+        my_put_square_matrix(framebuffer, 18, 50, 0, 0, sfRed, sfBlue, sfCyan, sfYellow);
+        //        my_put_square_matrix(framebuffer, 9, 100, 0, 0, sfRed, sfBlue, sfCyan, sfYellow);
 
         /* Update the texture from the pixels array of the framebuffer */
         sfTexture_updateFromPixels(texture, framebuffer->pixels, framebuffer->width, framebuffer->height, 0, 0);
